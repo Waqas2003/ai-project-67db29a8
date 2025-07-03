b71244be3cab2178826475a973b5ed0a2a462562
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 01:15:39 +0500

    Add Flag.js

---
+++ Flag.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Flag.css';
+
+const Flag = () => {
+  return (
+    <div className="flag">
+      <div className="green" />
+      <div className="white">
+        <div className="crescent" />
+        <div className="star" />
+      </div>
+    </div>
+  );
+};
+
+export default Flag;
